404d883810c8c2bef109ee48e63f330c85e596d7
ksenia1lvova <ksenia.lvova@ivkhk.ee>
2019-10-02 15:03:57 +0300

    hello

--- public_html/Z_game/myJS.js
+++ public_html/Z_game/myJS.js
@@ -47,3 +47,164 @@ function prepareGameField() {
 
 	document.getElementById('snake-field').appendChild(game_table);
 }
+
+
+
+function startGame() {
+	gameIsRunning = true;
+	respawn();
+
+	snake_timer = setInterval(move, SNAKE_SPEED);
+	setTimeout(createFood, 5000);
+}
+
+
+
+function respawn() {
+
+	var start_coord_x = Math.floor(FIELD_SIZE_X / 2);
+	var start_coord_y = Math.floor(FIELD_SIZE_Y / 2);
+
+	var snake_head = document.getElementsByClassName('cell-' + start_coord_y + '-' + start_coord_x)[0];
+	snake_head.setAttribute('class', snake_head.getAttribute('class') + 'snake-unit');
+
+	var snake_tail = document.getElementsByClassName('cell-' + (start_coord_y - 1) + '-' + start_coord_x)[0];
+	snake_tail.setAttribute('class', snake_tail.getAttribute('class') + 'snake-unit');
+
+	snake.push(snake_head);
+	snake.push(snake_tail);
+}
+
+
+
+function move() {
+
+	var snake_head_classes = snake(snake.length - 1).getAttribute('class').split(' ');
+
+	var new_unit;
+	var snake_coords = snake_head_classes[1].split('-');
+	var coord_y = parseInt(snake_coords[1]);
+	var coord_x = parseInt(snake_coords[2]);
+
+	if (direction == 'x-') {
+		new_unit = document.getElementsByClassName('cell-' + (coord_y) + '-' +(coord_x - 1))[0];
+	}
+	else if (direction == 'x+') {
+		new_unit = document.getElementsByClassName('cell-' + (coord_y) + '-' +(coord_x + 1))[0];
+	}
+	else if (direction == 'y+') {
+		new_unit = document.getElementsByClassName('cell-' + (coord_y - 1) + '-' +(coord_x))[0];
+	}
+	else if (direction == 'y-') {
+		new_unit = document.getElementsByClassName('cell-' + (coord_y + 1) + '-' +(coord_x))[0];
+	}
+
+
+
+	if (!isSnakeUnit(new_unit) && new_unit !== undefined) {
+
+		new_unit.setAttribute('class', new_unit.getAttribute('class') + 'snake-unit');
+		snke.push(new_unit);
+
+		if(!haveFood(new_unit)) {
+			var removed = snake.splice(0, 1)[0];
+			var classes = removed.getAttribute('class').split(' ');
+
+			removed.setAttribute('class', classes[0] + ' ' + classes[1]);
+		}
+	}
+	else {
+		finishTheGame();
+	}
+}
+
+
+
+function isSnakeUnit(unit) {
+	var check = false;
+
+	if (snake.includes(unit)) {
+		check = true;
+	}
+	return check;
+}
+
+
+
+function haveFood(unit) {
+	var check = false;
+
+	var unit_classes = unit.getAttribute('class').split(' ');
+
+	if (unit_classes.includes('food-unit')) {
+		check = true;
+		createFood();
+
+		score++;
+	}
+	return check;
+}
+
+
+
+function createFood() {
+	var foodCreated = false;
+
+	while (!foodCreated) {
+
+		var food_x = Math.floor(Math.random() * FIELD_SIZE_X);
+		var food_y = Math.floor(Math.random() * FIELD_SIZE_Y);
+
+		var food_cell = document.getElementsByClassName('cell-' + food_y + '-' + food_x)[0];
+		var food_cell_classes = food_cell.getAttribute('class').split(' ');
+
+		if (!food_cell_classes.includes('snake-unit')) {
+			var classes = '';
+			for (var i = 0; i < food_cell_classes.length; i++) {
+				classes += food_cell_classes[i] + ' ';
+			}
+			food_cell.setAttribute('class', classes + 'food-unit');
+			foodCreated = true;
+		}
+	}
+}
+
+
+
+function changeDirection(e) {
+	console.log(e);
+	switch (e.keyCode) {
+		case 37:
+			if (direction != 'x+') {
+				direction = 'x-'
+			}
+			break;
+		case 38:
+			if (direction != 'y-') {
+				direction = 'y+'
+			}
+		case 39:
+			if (direction != 'x-') {
+				direction = 'x+'
+			}
+		case 40:
+			if (direction != 'y+') {
+				direction = 'y-'
+			}
+			break;
+	}
+}
+
+
+
+function finishTheGame() {
+	gameIsRunning = false;
+	clearInterval(snake_timer);
+	alert('Вы проиграли! Ваш результат: ' + score.toString())
+}
+
+function refreshGame() {
+	location.reload();
+}
+
+window.onload = init;
